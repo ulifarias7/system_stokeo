e4256b7c74b80bf80f5af16bbe9db56dcce8f99d
ulises farias <fariasulisesnicolas@gmail.com>
2025-05-31 20:34:42 -0300

    migraciones de Producto

--- SistemaStokeo.DAL/EntityConfiguration/ProductoConfiguration.cs
+++ SistemaStokeo.DAL/EntityConfiguration/ProductoConfiguration.cs
@@ -8,25 +8,25 @@ namespace SistemaStokeo.DAL.EntityConfiguration
     {
         public void Configure(EntityTypeBuilder<Producto> builder)
         {
+            builder.ToTable("producto");
+
             builder.HasKey(e => e.IdProducto).HasName("PK__Producto__07F4A1320141252F");
 
-            builder.ToTable("Producto");
+            builder.Property(e => e.IdProducto).HasColumnName("id_producto");
 
-            builder.Property(e => e.IdProducto).HasColumnName("idProducto");
             builder.Property(e => e.EsActivo)
                 .HasDefaultValueSql("((1))")
-                .HasColumnName("esActivo");
+                .HasColumnName("es_activo");
 
             builder.Property(e => e.FechaRegistro)
                 .HasDefaultValueSql("(getdate())")
                 .HasColumnType("datetime")
-                .HasColumnName("fechaRegistro");
+                .HasColumnName("fecha_registro");
 
-            builder.Property(e => e.IdCategoria).HasColumnName("idCategoria");
+            builder.Property(e => e.IdCategoria).HasColumnName("id_categoria");
            
             builder.Property(e => e.Nombre)
                 .HasMaxLength(100)
-                .IsUnicode(false)
                 .HasColumnName("nombre");
 
             builder.Property(e => e.Precio)
@@ -35,7 +35,8 @@ namespace SistemaStokeo.DAL.EntityConfiguration
 
             builder.Property(e => e.Stock).HasColumnName("stock");
 
-            builder.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+            builder.HasOne(d => d.IdCategoriaNavigation)
+                .WithMany(p => p.Productos)
                 .HasForeignKey(d => d.IdCategoria)
                 .HasConstraintName("FK__Producto__idCate__49C3F6B7");
         }
---
+++ SistemaStokeo.DAL/Migrations/20250531232806_CreateMigrationsTablaProducto.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaStokeo.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMigrationsTablaProducto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Producto",
+                newName: "producto");
+
+            migrationBuilder.RenameColumn(
+                name: "idCategoria",
+                table: "producto",
+                newName: "id_categoria");
+
+            migrationBuilder.RenameColumn(
+                name: "fechaRegistro",
+                table: "producto",
+                newName: "fecha_registro");
+
+            migrationBuilder.RenameColumn(
+                name: "esActivo",
+                table: "producto",
+                newName: "es_activo");
+
+            migrationBuilder.RenameColumn(
+                name: "idProducto",
+                table: "producto",
+                newName: "id_producto");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Producto_idCategoria",
+                table: "producto",
+                newName: "IX_producto_id_categoria");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nombre",
+                table: "producto",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldUnicode: false,
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "producto",
+                newName: "Producto");
+
+            migrationBuilder.RenameColumn(
+                name: "id_categoria",
+                table: "Producto",
+                newName: "idCategoria");
+
+            migrationBuilder.RenameColumn(
+                name: "fecha_registro",
+                table: "Producto",
+                newName: "fechaRegistro");
+
+            migrationBuilder.RenameColumn(
+                name: "es_activo",
+                table: "Producto",
+                newName: "esActivo");
+
+            migrationBuilder.RenameColumn(
+                name: "id_producto",
+                table: "Producto",
+                newName: "idProducto");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_producto_id_categoria",
+                table: "Producto",
+                newName: "IX_Producto_idCategoria");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nombre",
+                table: "Producto",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
