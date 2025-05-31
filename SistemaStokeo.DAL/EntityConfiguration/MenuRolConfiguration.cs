7493257287a4dde9125647d367ce77f18d3140e1
ulises farias <fariasulisesnicolas@gmail.com>
2025-05-31 20:17:07 -0300

    migraciones de MenuRol

--- SistemaStokeo.DAL/EntityConfiguration/MenuRolConfiguration.cs
+++ SistemaStokeo.DAL/EntityConfiguration/MenuRolConfiguration.cs
@@ -8,23 +8,23 @@ namespace SistemaStokeo.DAL.EntityConfiguration
     {
         public void Configure(EntityTypeBuilder<MenuRol> builder)
         {
-            
+            builder.ToTable("menu_rol");
+
             builder.HasKey(e => e.IdMenuRol).HasName("PK__MenuRol__9D6D61A45301E146");
 
-            builder.ToTable("MenuRol");
+            builder.Property(e => e.IdMenuRol).HasColumnName("id_menu_rol");
+            builder.Property(e => e.IdMenu).HasColumnName("id_menu");
+            builder.Property(e => e.IdRol).HasColumnName("id_rol");
 
-            builder.Property(e => e.IdMenuRol).HasColumnName("idMenuRol");
-            builder.Property(e => e.IdMenu).HasColumnName("idMenu");
-            builder.Property(e => e.IdRol).HasColumnName("idRol");
-
-            builder.HasOne(d => d.IdMenuNavigation).WithMany(p => p.MenuRols)
+            builder.HasOne(d => d.IdMenuNavigation)
+                .WithMany(p => p.MenuRols)
                 .HasForeignKey(d => d.IdMenu)
                 .HasConstraintName("FK__MenuRol__idMenu__3D5E1FD2");
 
-            builder.HasOne(d => d.IdRolNavigation).WithMany(p => p.MenuRols)
+            builder.HasOne(d => d.IdRolNavigation)
+                .WithMany(p => p.MenuRols)
                 .HasForeignKey(d => d.IdRol)
                 .HasConstraintName("FK__MenuRol__idRol__3E52440B");
-            
         }
     }
 }
---
+++ SistemaStokeo.DAL/Migrations/20250531231444_CreateMigrationsTablaMenuRol.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaStokeo.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMigrationsTablaMenuRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "MenuRol",
+                newName: "menu_rol");
+
+            migrationBuilder.RenameColumn(
+                name: "idRol",
+                table: "menu_rol",
+                newName: "id_rol");
+
+            migrationBuilder.RenameColumn(
+                name: "idMenu",
+                table: "menu_rol",
+                newName: "id_menu");
+
+            migrationBuilder.RenameColumn(
+                name: "idMenuRol",
+                table: "menu_rol",
+                newName: "id_menu_rol");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MenuRol_idRol",
+                table: "menu_rol",
+                newName: "IX_menu_rol_id_rol");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MenuRol_idMenu",
+                table: "menu_rol",
+                newName: "IX_menu_rol_id_menu");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "menu_rol",
+                newName: "MenuRol");
+
+            migrationBuilder.RenameColumn(
+                name: "id_rol",
+                table: "MenuRol",
+                newName: "idRol");
+
+            migrationBuilder.RenameColumn(
+                name: "id_menu",
+                table: "MenuRol",
+                newName: "idMenu");
+
+            migrationBuilder.RenameColumn(
+                name: "id_menu_rol",
+                table: "MenuRol",
+                newName: "idMenuRol");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_menu_rol_id_rol",
+                table: "MenuRol",
+                newName: "IX_MenuRol_idRol");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_menu_rol_id_menu",
+                table: "MenuRol",
+                newName: "IX_MenuRol_idMenu");
+        }
+    }
+}
